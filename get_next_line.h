9de6ed8b972eead3003b3ebe6186d9287841b524
Wayn Venita <wvenita@il-f6.21-school.ru>
2019-04-24 23:06:45 +0300

    final edition

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,19 @@
 /*   By: wvenita <wvenita@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 21:06:49 by wvenita           #+#    #+#             */
-/*   Updated: 2019/04/16 23:32:57 by wvenita          ###   ########.fr       */
+/*   Updated: 2019/04/24 22:56:50 by wvenita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft/libft.h"
+# include <sys/types.h>
 # include <fcntl.h>
-# define BUFF_SIZE 32
+# include <fcntl.h>
+# define BUFF_SIZE 8
 
 int		get_next_line(const int fd, char **line);
+int		write_next_line(const int fd, char **line, char **str);
 
 #endif
